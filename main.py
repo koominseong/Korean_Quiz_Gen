96f61f5afd869fc0abc2f5dda486b3bd20b11261
Minseong Koo <bestms8921@gmail.com>
2025-02-02 21:44:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from openai import OpenAI
 
 
 # OpenAI API 키 설정
-OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
-client = OpenAI(api_key=OPENAI_API_KEY)
+#OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 def generate_questions(content, category, difficulty, num_questions):
     prompt = f"""- 종류: {category}
